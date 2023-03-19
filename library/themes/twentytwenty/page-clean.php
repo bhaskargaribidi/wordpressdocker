2d0ae74dc8168362765a1dd1a6427044e7edbc6d
Bhaskar Garibidi <bhaskargaribidi@gmail.com>
2023-03-19 13:26:57 +0530

    WordPress Docker Repository

---
+++ library/themes/twentytwenty/page-clean.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Template Name: Clean Page
+ * This template will only display the content you entered in the page editor
+ */
+?>
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <?php wp_head(); ?>
+    <style type="text/css">
+        iframe{height: 100vh;}
+    </style>
+</head>
+<body class="cleanpage">
+<?php
+    while ( have_posts() ) : the_post();  
+        the_content();
+    endwhile;
+?>
+<?php wp_footer(); ?>
+</body>
+</html>
